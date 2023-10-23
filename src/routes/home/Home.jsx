56874697570c1a37a89a3ca90f4d4e9f50a27c2e
abduazim <119504866+abduazimkh@users.noreply.github.com>
2023-10-23 16:20:34 +0500

    commited

--- src/routes/home/Home.jsx
+++ src/routes/home/Home.jsx
@@ -1,10 +1,13 @@
+import { useEffect, useState } from 'react';
+import { apiInstance } from '../../api';
+import { Link } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 import "./Home.css";
-import { apiInstance } from "../../api";
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 
 const Home = () => {
-  const [page, setPage] = useState(1);
+  const location = useLocation();
+  const navigate = useNavigate();
+  const [page, setPage] = useState(+new URLSearchParams(location.search).get("offset") || 1);
 
   const [apiResult, setApiResult] = useState({
     data: [],
@@ -13,63 +16,73 @@ const Home = () => {
     total: 0
   });
 
+
   useEffect(() => {
-    async function loadData() {
-      try {
-        const response = await apiInstance(
-          `/products/?offset=${(page - 1) * 12}&limit=12`
-        );
+    async function loadData(){
+      try{
+        const response = await apiInstance(`/products/?offset=${( page - 1) * 12}&limit=12`);
         const total = await apiInstance("/products");
         setApiResult({
           data: response.data,
           loading: false,
           error: false,
           total: total.data.length
-        });
-      } catch (error) {
+        })
+      }
+      catch(error){
         setApiResult({
           data: [],
           loading: false,
           error: true,
           total: 0
-        });
+        })
       }
     }
+
     loadData();
-  }, [page]);
+  }, [page])
 
-  function addPage() {
+  const search = new URLSearchParams();
+
+  useEffect(() => {
+    search.append("offset",page);
+    let k =  search.toString();
+    navigate(`?${k}`)
+  }, [page])
+
+  const addPage = () => {
     if(page < Math.ceil(apiResult.total / 12)){
-        setPage(page + 1);
+     setPage(page + 1)
     }
   }
 
-  function substractPage() {
-    if (page > 1) {
-      setPage(page - 1);
+  const substractPage = () => {
+    if(page > 1){
+      setPage(page - 1)
     }
   }
 
-  return apiResult.loading ? (
-    <p>Loading...</p>
-  ) : (
-    <div className="cards">
-      {apiResult.data.map((card) => {
-        return (
-          <Link to={`/product-view/${card.id}`} className="card" key={card.id}>
-            <img src={card.images[0]} alt="" />
-            <h3>{card.title}</h3>
-            <p>{card.description.slice(0, 40)}</p>
-            <strong>${card.price}</strong>
-          </Link>
-        );
-      })}
+  return (
+    <div className='cards'>
+        {
+          apiResult.loading ? <p>Loading...</p> :
+          apiResult.data.map((card) => {
+            return (
+                <Link to={`/product-view/${card.id}`} className="card" key={card.id}>
+                  <img src={card.images[0]} alt="" />
+                  <h3>{card.title}</h3>
+                  <p>{card.description.slice(0, 40)}</p>
+                  <strong>${card.price}</strong>
+                </Link>
+              )
+          })
+        }
 
-      <button onClick={substractPage}>-</button>
-      {page}
-      <button onClick={addPage}>+</button>
+        <button onClick={substractPage}>-</button>
+        {page}
+        <button onClick={addPage}>+</button>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
