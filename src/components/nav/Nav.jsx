b257690acc65289342775eef30fda9a9194deb6b
abduazim <119504866+abduazimkh@users.noreply.github.com>
2023-10-24 11:38:12 +0500

    commited

---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom";
+import "./Nav.css";
+import { useLocation } from "react-router-dom"
+
+
+const Nav = () => {
+  const ACCESS_TOKEN = localStorage.getItem("user-token");
+  const { pathname } = useLocation();
+
+  return !pathname.includes("login") && !pathname.includes("signup") && (
+    <div className="nav__wrapper">
+      <form id="form">
+        <input id="search" type="text" placeholder="Search" />
+
+        <select id="select">
+          <option selected disabled>
+            all
+          </option>
+        </select>
+      </form>
+
+      {!ACCESS_TOKEN && (
+        <div className="register">
+          <Link to="/signup">Sign Up</Link>
+          <Link to="/login">Log In</Link>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Nav;
