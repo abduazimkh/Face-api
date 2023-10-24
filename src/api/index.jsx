158f6834c656f24e1d641f090724996b0e4f402d
abduazim <119504866+abduazimkh@users.noreply.github.com>
2023-10-24 11:36:43 +0500

    commited

--- src/api/index.jsx
+++ src/api/index.jsx
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const apiInstance = axios.create({
-    baseURL: import.meta.env.VITE_API,
-    headers: {
-        "Content-Type":"application/json"
-    },
-    timeout: 1000,
-})
+  baseURL: import.meta.env.VITE_API,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  timeout: 10000,
+});
 
 export { apiInstance };
