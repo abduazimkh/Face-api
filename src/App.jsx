7afc94ed64be4985948f172a7fb729cb9370c497
abduazim <119504866+abduazimkh@users.noreply.github.com>
2023-10-24 11:36:54 +0500

    commited

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,18 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./routes/home/Home";
 import Signup from "./routes/signup/Signup";
 import ProductView from "./routes/product-view/ProductView";
+import Nav from "./components/nav/Nav";
+import Login from "./routes/login/Login";
 
 function App() {
   return (
     <>
+      <Nav/>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="signup" element={<Signup />} />
-        <Route path="product-view/:id" element={<ProductView />} />
+        <Route path="/" exact element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/product-view/:id" element={<ProductView />} />
       </Routes>
     </>
   );
