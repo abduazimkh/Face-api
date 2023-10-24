48bb93c8669946dcfdedaf869a0bfb6ad6e83f6c
abduazim <119504866+abduazimkh@users.noreply.github.com>
2023-10-24 21:39:10 +0500

    loader added suspens and lezy

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { Suspense, lazy } from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom';
+import Loader from './components/loader/Loader';
+const App = lazy(() => import("./App"));
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <BrowserRouter>
-      <App />
+      <Suspense fallback={<Loader/>}>
+        <App />        
+      </Suspense>
     </BrowserRouter>
 )
