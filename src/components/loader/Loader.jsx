43ded539799ef37ddeedf6e7e600e32cc295918b
abduazim <119504866+abduazimkh@users.noreply.github.com>
2023-10-24 21:37:18 +0500

    loader added

---
+++ src/components/loader/Loader.jsx
@@ -0,0 +1,12 @@
+import './Loader.scss';
+
+
+const Loader = () => {
+  return (
+    <div className='loader__wrapper'>
+        <div class="lds-facebook"><div></div><div></div><div></div></div>
+    </div>
+  )
+}
+
+export default Loader
