4c4acd383888bd8e0c2a9119d19632fe379b3bf5
abduazim <119504866+abduazimkh@users.noreply.github.com>
2023-10-24 11:41:32 +0500

    login commited

---
+++ src/routes/login/Login.jsx
@@ -0,0 +1,62 @@
+import './Login.css'
+import { useEffect, useRef, useState } from "react";
+import signUp from "../../assets/images/signup.svg";
+import { apiInstance } from "../../api";
+import axios from "axios";
+import { useNavigate, Navigate } from 'react-router-dom';
+
+
+
+const Login = () => {
+  const navigate = useNavigate();
+
+  const [userEmail, setUserEmail] = useState("");
+  const [userPassword, setUserPassword] = useState("");
+
+  async function userRegister(e) {
+    e.preventDefault();
+    try {
+      const response = await apiInstance.post(`/auth/login`, {
+        email: userEmail,
+        password: userPassword,
+      });
+      if(response.data.access_token){
+        console.log(response);
+        localStorage.setItem("user-token", response.data.access_token)
+        navigate("/");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+
+  return (
+    <div className="login">
+      <div>
+        <img src={signUp} alt="" />
+      </div>
+      <form onSubmit={userRegister} id="login-form">
+        <h2>Log In</h2>
+        <input
+          placeholder="Email"
+          onChange={(e) => setUserEmail(e.target.value)}
+          value={userEmail}
+          type="text"
+        />
+        <input
+          placeholder="Password"
+          onChange={(e) => setUserPassword(e.target.value)}
+          value={userPassword}
+          type="password"
+        />
+
+        <button id="btn" type="submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default Login
