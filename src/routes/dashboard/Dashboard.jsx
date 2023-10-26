fed938edf166280f8d2eeb2dd74b5f697d8ca5da
abduazim <119504866+abduazimkh@users.noreply.github.com>
2023-10-26 12:21:09 +0500

    product view comited

--- src/routes/dashboard/Dashboard.jsx
+++ src/routes/dashboard/Dashboard.jsx
@@ -1,38 +1,60 @@
-import { Outlet, Link, NavLink } from 'react-router-dom'
-import './Dashboard.scss'
-import { TbSettings2 } from 'react-icons/tb'
-import { HiOutlineSquare3Stack3D } from 'react-icons/hi2'
-import { TbUserSquareRounded } from 'react-icons/tb';
+import { Outlet, Link, NavLink } from "react-router-dom";
+import "./Dashboard.scss";
+import { TbSettings2 } from "react-icons/tb";
+import { HiOutlineSquare3Stack3D } from "react-icons/hi2";
+import { TbUserSquareRounded } from "react-icons/tb";
+import { useState } from "react";
+import Login from '../login/Login';
 
 const Dashboard = () => {
-  return (
-    <div className='dashboard__wrapper'>
+  const [asidebar, setAsidebar] = useState(true);
+
+  function asideBar() {
+    setAsidebar(!asidebar);
+  }
+
+  return localStorage.getItem("user-token")? (
+    <div className="dashboard__wrapper">
       <aside>
-        <div  className="dashboard-title">
+        <div className="dashboard-title" style={asidebar ? { width: "30px" } : {padding:" 36px 28px"}}
+        onClick={asideBar}>
           <li>
-            <TbSettings2/>
+            <TbSettings2 />
           </li>
-          <h1>Dashboard</h1>
-        </div>
-        <div className='menu-item'>
-              
-          <NavLink className={({isActive}) => isActive ? 'link link--active':'link'} to="/dashboard/products">
-          <li><HiOutlineSquare3Stack3D/></li>
-           Products</NavLink>
-            
-          <NavLink className={({isActive}) => isActive ? 'link link--active':'link'} to="/dashboard/customers">
-          <li><TbUserSquareRounded/></li>
-          Customers</NavLink>
+          <h1 style={asidebar ? { display: "none"} : {display: "block"}}>Dashboard</h1>
+        </div >
+        <div  className="menu-item" style={asidebar ? { display: "none"} : {display: "block"}}>
+          <NavLink 
+            className={({ isActive }) =>
+              isActive ? "link link--active" : "link"
+            }
+            to="/dashboard/products" 
+          >
+            <li>
+              <HiOutlineSquare3Stack3D />
+            </li>
+            Products
+          </NavLink>
+
+          <NavLink
+            className={({ isActive }) =>
+              isActive ? "link link--active" : "link"
+            }
+            to="/dashboard/customers"
+          >
+            <li>
+              <TbUserSquareRounded />
+            </li>
+            Customers
+          </NavLink>
         </div>
       </aside>
-      
 
       <div className="dashboard__main">
-        <Outlet/>
+        <Outlet />
       </div>
-
     </div>
-  )
-}
+  ) : <Login to="/login"/>
+};
 
-export default Dashboard
+export default Dashboard;
