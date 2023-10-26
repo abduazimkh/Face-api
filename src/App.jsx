3f01c73b628e80431e61afae6e8e4a01eac7e6d6
abduazim <119504866+abduazimkh@users.noreply.github.com>
2023-10-26 12:19:05 +0500

    comited

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import Nav from "./components/nav/Nav";
 import Login from "./routes/login/Login";
 import User from "./routes/user/User";
 import Dashboard from "./routes/dashboard/Dashboard";
+import Product from "./routes/dashboard/product/Product";
+import Customers from "./routes/dashboard/customers/Customers";
 
 function App() {
   return (
@@ -17,7 +19,10 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/user" element={<User />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard" element={<Dashboard />} >
+          <Route path="products" element={<Product />}/>
+          <Route path="customers" element={<Customers />} />
+        </Route>
         <Route path="/product-view/:id" element={<ProductView />} />
       </Routes>
     </>
